00ee18c0867333af304c3f38816b7bf7e5c2a161
Arman Hovhannisyan <armanhovhannisyan1002@gmail.com>
2021-01-30 16:55:53 +0400

    first commit

---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookListMVC.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+    }
+}
